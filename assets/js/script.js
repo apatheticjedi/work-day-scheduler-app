8d9f48f7146791eb1c1b3a63679f2331941273af
Duder <thedudeabides1981@hotmail.com>
2022-08-13 16:07:14 -0700

    streamlined checkTime function, added localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,105 +4,42 @@ var hour = moment().hour();
 // display current day
 $('#currentDay').text(now);
 
-function checkTime() {
-    if (hour > 8) {
-        $('#eightA').addClass('past');
-    } else if (hour === 8) {
-        $('#eightA').addClass('present')
-    } else {
-        $('#eightA').addClass('future')
-    };
- 
-    if (hour > 9) {
-        $('#nineA').addClass('past');
-    } else if (hour === 9) {
-        $('#nineA').addClass('present')
-    } else {
-        $('#nineA').addClass('future')
-    };
+// set description backgroung color
+function checkTime(){
+    $(".description").each(function(){
+        var id = $(this).attr("id");
 
-    if (hour > 10) {
-        $('#tenA').addClass('past');
-    } else if (hour === 10) {
-        $('#tenA').addClass('present')
-    } else {
-        $('#tenA').addClass('future')
-    };
-
-    if (hour > 11) {
-        $('#elevenA').addClass('past');
-    } else if (hour === 11) {
-        $('#elevenA').addClass('present')
-    } else {
-        $('#elevenA').addClass('future')
-    };
-
-    if (hour > 12) {
-        $('#noon').addClass('past');
-    } else if (hour === 12) {
-        $('#noon').addClass('present')
-    } else {
-        $('#noon').addClass('future')
-    };
-
-    if (hour > 13) {
-        $('#oneP').addClass('past');
-    } else if (hour === 13) {
-        $('#oneP').addClass('present')
-    } else {
-        $('#oneP').addClass('future')
-    };
-
-    if (hour > 14) {
-        $('#twoP').addClass('past');
-    } else if (hour === 14) {
-        $('#twoP').addClass('present')
-    } else {
-        $('#twoP').addClass('future')
-    };
-
-    if (hour > 15) {
-        $('#threeP').addClass('past');
-    } else if (hour === 15) {
-        $('#threeP').addClass('present')
-    } else {
-        $('#threeP').addClass('future')
-    };
-
-    if (hour > 16) {
-        $('#fourP').addClass('past');
-    } else if (hour === 16) {
-        $('#fourP').addClass('present')
-    } else {
-        $('#fourP').addClass('future')
-    };
-
-    if (hour > 17) {
-        $('#fiveP').addClass('past');
-    } else if (hour === 17) {
-        $('#fiveP').addClass('present')
-    } else {
-        $('#fiveP').addClass('future')
-    };
+        if(id == hour){
+            $(this).addClass("present");
+        }else if(id < hour){
+            $(this).addClass("past");
+        }else{
+            $(this).addClass("future");
+        }
+    })
 };
 
-function enterText() {
-    
-}
+// save to localStorage
+$(".saveBtn").click(function(e){
+e.preventDefault();
+var toDo =$(this).siblings('.description')
+.val();
+var time = $(this).siblings('.description')
+.attr('id');
 
-$(".description").on("click", function() {
-    var text = $(this)
-    .text()
-    .trim();
-
-    var textInput = $("<textarea>")
-    .val(text);
-
-    var saveBtn = $(".saveBtn");
-
-    $(this).replaceWith(textInput);
+localStorage.setItem(time, toDo);
 });
 
-setInterval(checkTime(), 300000);
+// load from localStorage
+function loadTasks(){
+    for(let i = 8; i < 18; i++){
+        var toDo = localStorage.getItem(i);
+        $("#"+i).val(toDo);
+    }
+};
+
+// check time every 5 minutes
+setInterval(checkTime, 300000);
 
 checkTime();
+loadTasks();
